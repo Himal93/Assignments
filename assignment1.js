ce49c8709aeaae87d0f382e887fa62b07cecf1fa
Himal93 <himalranamgr@gmail.com>
2025-04-06 21:46:39 +0545

    moved isExpired function inside object

--- assignment1.js
+++ assignment1.js
@@ -24,25 +24,39 @@ let y = 1;
 console.log("y is", ++y);
 
 // Create a JS object that represent a product name(string) and Expiry Date(Data object)
+// The object should have a method called isExpired() that: checks if product is expired ans returns true if it is expired else false
 let product = {
   ProductName: "Noodles",
   ExpiryDate: "Mar 25 2019",
   amount: Math.ceil(Math.random() * 100),
+  isExpired: function () {
+    if (new Date(this.ExpiryDate) < new Date()) {
+      console.log(`The  ${product.ProductName} is expired`);
+    } else {
+      console.log(`The  ${product.ProductName} is not expired`);
+    }
+  },
 };
 console.log(product);
-let currentDate = new Date();
+// let currentDate = new Date();
 
-var isExpired = (product, currentDate) => {
-  if (new Date(product.ExpiryDate) < currentDate) {
-    console.log(`The  ${product.ProductName} is expired`);
-  } else {
-    console.log(`The  ${product.ProductName} is not expired`);
-  }
-};
+// var isExpired = (product, currentDate) => {
+//   if (new Date(product.ExpiryDate) < currentDate) {
+//     console.log(`The  ${product.ProductName} is expired`);
+//   } else {
+//     console.log(`The  ${product.ProductName} is not expired`);
+//   }
+// };
 
-isExpired(product, currentDate);
+// isExpired(product, currentDate);
+product.isExpired();
 
-// Create function called checkStock(product) that returns a Promise
+// Create function called checkStock(product) that returns a Promise. This function should simulate checking if a product
+// is in stock br using Math.randon() to determine sucess or failure. If value is even, assume product is in stock else not.
+// Promise should:
+//    Resolve after 2 sec with message:"Product [productname] is available!"
+//    Reject after 2 sec with error message: "Sorry, [product name] is out of stock!"
+// Use both async/await and the...catch syntax for checking
 checkStock = async (product) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
